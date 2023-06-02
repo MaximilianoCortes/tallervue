10ea27751180e1a24067d87d20da8af55a543f71
CristobalMatus <c.matus10@ufromail.cl>
2023-06-02 16:21:20 -0400

    ola

--- tallervue/src/components/ProductoComponente.vue
+++ tallervue/src/components/ProductoComponente.vue
@@ -2,10 +2,10 @@
     <div v-if="producto" class="card">
   <img :src="producto.images[0]" alt="Producto" style="width:15%">
   <div class="container">
-    <h4><b>{{producto.name}}</b></h4>
+    <h2><b>{{producto.name}}</b></h2>
     <p> {{producto.description}}</p>
-    <p><b>{{producto.createdAt}}</b></p>
-    <p><b> ${{producto.price}}</b></p>
+    <p><b>Fecha publicación: {{producto.createdAt}}</b></p>
+    <p><b>Precio: ${{producto.price}}</b></p>
     <!-- <button><router-link :to="/VistaDetalleProducto/`${producto._id}`">Ver detalles</router-link></button> -->
   </div>
 </div>
