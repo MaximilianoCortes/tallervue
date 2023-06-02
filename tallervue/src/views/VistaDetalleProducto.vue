be5d95f82539bce2fece5b1576f073eb3c3862de
MaximilianoCortes <m.cortes12@ufromail.cl>
2023-06-02 15:10:21 -0400

    Update VistaDetalleProducto.vue

--- tallervue/src/views/VistaDetalleProducto.vue
+++ tallervue/src/views/VistaDetalleProducto.vue
@@ -1,15 +1,22 @@
 <template>
-    <div>
-      <ProductoComponente/>
-    </div>
-  </template>
-  
-  <script>
-import ProductoComponente from '../views/VistaProducto';
-  
-  export default {
-    components: {
-      ProductoComponente
-    }
-  };
-  </script>
+  <div>
+    <!-- Obtener el producto desde tu API o fuente de datos -->
+    <detalle-producto-componente :producto="producto"></detalle-producto-componente>
+  </div>
+</template>
+
+<script>
+import DetalleProductoComponente from '../components/DetalleProductoComponente.vue';
+
+export default {
+  components: {
+    DetalleProductoComponente
+  },
+  data() {
+    return {
+      producto: {} // Aquí debes obtener el producto desde tu API o fuente de datos
+    };
+  },
+  // Aquí puedes agregar métodos para obtener los datos del producto
+};
+</script>
