09bb4ab0bcaa0b5cd0eee52cdf125b943ae9b234
MaximilianoCortes <m.cortes12@ufromail.cl>
2023-06-02 15:04:41 -0400

    listo

--- tallervue/src/views/VistaProducto.vue
+++ tallervue/src/views/VistaProducto.vue
@@ -1,11 +1,12 @@
 <template>
     <div>
+      <h1>hola</h1>
       <ProductoComponente/>
     </div>
   </template>
   
   <script>
-import ProductoComponente from '../views/VistaProducto';
+import ProductoComponente from '../components/ProductoComponente.vue';
   
   export default {
     components: {
