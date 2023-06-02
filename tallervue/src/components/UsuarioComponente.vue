63e7ef11f1fce09fd267e5be11cebe276df01d7a
MaximilianoCortes <m.cortes12@ufromail.cl>
2023-06-02 14:47:03 -0400

    ta la base de la tarea
    agregue como base la tarea que hice de vue

---
+++ tallervue/src/components/UsuarioComponente.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="center-container">
+        <h1>TU IP ES : <router-link :to="`/DoxxeoInfo/${ip}`">{{ ip }}</router-link></h1>
+        <div>Haz click en tu ip para mas informacion</div>
+  </div>
+  </template>
+  
+  <script>
+  import axios from 'axios'
+  export default {
+    data() {
+      return {
+        ip: ''
+      };
+    },
+    created() {
+      this.obtenerIP();
+    },
+    methods: {
+      obtenerIP() {
+        axios
+          .get('https://api.ipify.org/?format=json')
+          .then(response => {
+            this.ip = response.data.ip;
+          })
+          .catch(error => {
+            console.error(error);
+          });
+      }
+    }
+  };
+  </script>
---
+++ tallervue/src/views/VistaUsuario.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+      <MostrarIp/>
+    </div>
+  </template>
+  
+  <script>
+  import MostrarIp from '../components/MostrarIp.vue';
+  
+  export default {
+    components: {
+      MostrarIp
+    }
+  };
+  </script>
